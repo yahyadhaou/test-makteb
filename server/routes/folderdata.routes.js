a25deeca3849e0a6be8ebfc35579e7e2211de9fc
yahyadhaou <yahya.dhaou75@gmail.com>
2023-12-22 11:15:51 +0100

    done

--- server/routes/folderdata.routes.js
+++ server/routes/folderdata.routes.js
@@ -8,9 +8,9 @@ const {
   insertfolderdata
   } = require("../controllers/folderdata.controller");
 
-  router.get("/getdata/:id", getdata);
-  router.post("/InsertFolderData", insertfolderdata);
-  router.get("/number/:numeroDossier", getcasebynumber)
-  router.put("/archiver", archive);
-    router.put("/updateDetails/:numeroDossier",updateContractData)
-    module.exports = router;
+  router.get("/getdata/:id", getdata);//done  
+  router.post("/InsertFolderData", insertfolderdata);//done
+  router.get("/number/:numeroDossier", getcasebynumber)//done
+  router.put("/archiver", archive);//done
+    router.put("/updateDetails/:numeroDossier",updateContractData)//done
+  module.exports = router;
